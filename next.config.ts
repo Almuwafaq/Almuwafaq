a8a6f73dfd39f5e0545bcb6b77eaa8a237346df8
Toriola Taofeek <toriolataofeekolamilekan@gmail.com>
2025-07-23 19:36:50 +0100

    fix - 404 page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  srcDir: "src", // ✅ this is all you need to make /src/app/page.tsx work
 };
 
 export default nextConfig;
